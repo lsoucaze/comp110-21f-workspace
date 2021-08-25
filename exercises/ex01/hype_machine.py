2cda531f96fb9cb3ab8bdf5bc82a7514bb13835c
Lilliana Soucaze <lilihope@ad.unc.edu>
2021-08-25 17:16:28 -0400

    Take 2?

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -4,6 +4,7 @@
 
 # TODO: Implement your program logic here
 
+__author__ = 730383737
 name: str = input("What is your name? ")
 print(name + ", you are my sunshine")
 print("I'm so proud of you " + name + "!")
