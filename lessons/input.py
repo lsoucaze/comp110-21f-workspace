3267bf5e3d2e650c9561f8dbd1e41c2ad25ab1eb
Lilliana Soucaze <lilihope@ad.unc.edu>
2021-08-26 12:31:46 -0400

    class 8/26

---
+++ lessons/input.py
@@ -0,0 +1,5 @@
+"""Demonstrates asking the user for input."""
+
+user_name: str = input("Waht is your name? ")
+print("Hello, " + user_name + ", good morning! ")
+print("You are awesome, " + user_name)
