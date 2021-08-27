74396d7aed279ac111b1d1c2fec684b7e995c757
Lilliana Soucaze <lilihope@ad.unc.edu>
2021-08-26 22:31:11 -0400

    efficiency, not graded

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -3,17 +3,17 @@
 __author__ = "730383737"
 
 
-lside: str = input("Number variable 1? ")
-rside: str = input("Number variable 2? ")
-lsint = int(lside)
-rsint = int(rside)
-a = str(lsint ** rsint)
-b = str(lsint / rsint)
-c = str(lsint // rsint)
-d = str(lsint % rsint)
-print("Left-hand side: " + lside)
-print("Right-hand side: " + rside)
-print(lside + " ** " + rside + " is " + a)
-print(lside + " / " + rside + " is " + b)
-print(lside + " // " + rside + " is " + c)
-print(lside + " % " + rside + " is " + d)
+lside = int(input("Number variable 1? "))
+rside = int(input("Number variable 2? "))
+a = lside ** rside
+b = lside / rside
+c = lside // rside
+d = lside % rside
+lside = str(lside)
+rside = str(rside)
+print("Left-hand side: " + (lside))
+print("Right-hand side: " + (rside))
+print((lside) + " ** " + (rside) + " is " + str(a))
+print((lside) + " / " + (rside) + " is " + str(b))
+print((lside) + " // " + (rside) + " is " + str(c))
+print((lside) + " % " + (rside) + " is " + str(d))
