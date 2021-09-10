f628aaebe1319ab325443db448c07fedfbe116dc
Lilliana Soucaze <lilihope@ad.unc.edu>
2021-09-09 22:04:36 -0400

    take 1

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,22 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730383737"
+
+word: str = (input("Enter a word: "))
+i = 0
+counter_two = 0
+duplicates = 0
+
+while i < len(word):
+    counter_two = i + 1
+    while counter_two < len(word):
+        if word[i] == word[counter_two]:
+            duplicates = duplicates + 1
+            counter_two = counter_two + 1
+        else:
+            counter_two = counter_two + 1
+    i = i + 1
+        
+variable = str(duplicates > 0)
+print(duplicates)
+print("Found duplicate: " + variable)
