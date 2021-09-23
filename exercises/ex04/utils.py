87ed3c0b6b34c0e383d5e4eaf78bab725339274c
Lilliana Soucaze <lilihope@ad.unc.edu>
2021-09-23 16:37:26 -0400

    Take 1

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,44 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730383737"
 
 
-# TODO: Implement your functions here.
+def all(haystack: list[int], needle: int) -> bool:
+    """Indicates whether or not all the ints in the list are the same as the given int."""
+    i: int = 0
+
+    while i < len(haystack):
+        item: int = haystack[i]
+        if item != needle:
+            return False
+        i += 1
+
+    return True
+
+
+def is_equal(list_1: list[int], list_2: list[int]) -> bool:
+    """Indicates whether 2 lists of int values are identical at every index value."""
+    i: int = 0
+
+    if len(list_1) == len(list_2):
+        while i < len(list_1):
+            if list_1[i] != list_2[i]:
+                return False
+            i += 1
+        return True
+    else:
+        return False
+
+
+def max(input: list[int]) -> int:
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+
+    i: int = 0
+    value: int = input[0]
+    
+    while i < len(input):
+        if input[i] >= value:
+            value: int = input[i]
+        i += 1
+    return value
