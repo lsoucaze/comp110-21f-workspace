0d96b7b210983f9f598d3f541c36ee044ce93527
Lilliana Soucaze <lilihope@ad.unc.edu>
2021-09-23 16:52:15 -0400

    ammended 'all', added doc string for 'max'

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -7,6 +7,8 @@ def all(haystack: list[int], needle: int) -> bool:
     """Indicates whether or not all the ints in the list are the same as the given int."""
     i: int = 0
 
+    if len(haystack) == 0:
+        return False
     while i < len(haystack):
         item: int = haystack[i]
         if item != needle:
@@ -31,6 +33,7 @@ def is_equal(list_1: list[int], list_2: list[int]) -> bool:
 
 
 def max(input: list[int]) -> int:
+    """Returns the max value of the list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
 
