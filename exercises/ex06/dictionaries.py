12a49944a00e2211700ebea6b59e72fc774fb104
Lilliana Soucaze <lilihope@ad.unc.edu>
2021-10-07 21:57:33 -0400

    EX06 Take 1

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,53 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730383737"
 
-# Define your functions below
+
+def invert(set_1: dict[str, str]) -> dict[str, str]:
+    """Inverts the keys and values of a dictionary."""
+    set_2 = dict()  # New flipped list
+    
+    for key in set_1:
+        if set_1[key] in set_2:
+            raise KeyError
+        else:
+            set_2[set_1[key]] = key
+    
+    return set_2
+
+
+def favorite_color(color_list: dict[str, str]) -> str:
+    """Returns the color that occurs most frequently."""
+    color: str = ""
+    color_count: int = 0
+    color_count_2: int = 0
+
+    for x in color_list:
+        color = color_list[x]
+
+        if color_list[x] == color:
+            color_count += 1
+        else:
+            color_count_2 += 1
+            color_2 = x
+
+    if color_count >= color_count_2:
+        return color
+    if color_count <= color_count_2:
+        return color_list[color_2]
+
+
+def count(input_list: list[str]) -> dict[str, int]:
+    final_set = dict()
+    
+    for fruit in input_list:  # I use fruit in tests. It makes sense I promise.
+        if fruit in final_set:
+            final_set[fruit] += 1
+        else:
+            final_set[fruit] = 1
+        
+    return final_set
+
+
+input_list = ["apple", "banana", "apple"]
+print(count(input_list))
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,61 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+# python -m pytest exercises/ex06
 
-__author__ = "123456789"
+from exercises.ex06.dictionaries import invert, favorite_color, count
+
+__author__ = "730383737"
+
+
+def test_invert_cats() -> None:
+    """Edge case: single entry."""
+    set_1 = {'apple': 'cat'}
+    assert invert(set_1) == {'cat': 'apple'}
+
+
+def test_invert_full_list() -> None:
+    """Use case: complex list."""
+    set_1 = {'a': 'z', 'b': 'y', 'c': 'x'}
+    assert invert(set_1) == {'z': 'a', 'y': 'b', 'x': 'c'}
+
+
+def test_invert_small_list() -> None:
+    """Use case: small list."""
+    set_1 = {'apple': 'red', 'banana': 'yellow', 'grape': 'purple'}
+    assert invert(set_1) == {'red': 'apple', 'yellow': 'banana', 'purple': 'grape'}
+
+
+def test_favorite_color_same() -> None:
+    """Edge case: Equal colors."""
+    color_list = {'Marc': 'Yellow', 'Ezri': 'Blue', 'Kris': 'Blue', 'Chris': 'Yellow', 'James': 'Blue', 'Lilly': 'Yellow'}
+    assert favorite_color(color_list) == "Yellow"
+
+
+def test_favorite_color_simple() -> None:
+    """Use case: three entries."""
+    color_list = {'Marc': 'Yellow', 'Ezri': 'Blue', 'Kris': 'Blue'}
+    assert favorite_color(color_list) == "Blue"
+
+
+def test_favorite_color_complex() -> None:
+    """Use case: simple five entries."""
+    color_list = {'Marc': 'Yellow', 'Ezri': 'Blue', 'Kris': 'Blue', 'Chris': 'Yellow', 'James': 'Blue'}
+    assert favorite_color(color_list) == "Blue"
+
+
+def test_count_complex() -> None:
+    """Edge case: no repeating entires."""
+    input_list = ["apple", "banana", "pear", "orange", "grape"]
+    assert count(input_list) == {'apple': 1, 'banana': 1, 'pear': 1, 'orange': 1, 'grape': 1}
+
+
+def test_count_simple() -> None:
+    """Use case: some repeating entires."""
+    input_list = ["apple", "banana", "apple", "orange", "apple"]
+    assert count(input_list) == {'apple': 3, 'banana': 1, 'orange': 1}
+
+
+def test_count_long() -> None:
+    """Use case: a lot of repeating entires."""
+    input_list = ["apple", "banana", "apple", "orange", "apple", "apple", "grape", "grape", "orange", "banana"]
+    assert count(input_list) == {'apple': 4, 'banana': 2, 'orange': 2, 'grape': 2}
